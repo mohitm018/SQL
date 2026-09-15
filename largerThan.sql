04f0e1493ff5d0c762f9568422babcaea372ab96
mohitm018 <91406180+mohitm018@users.noreply.github.com>
2026-09-15 13:32:15 +0530

    Add queries for films and high-salary employees
    Add SQL queries to retrieve films and employees based on cost and salary criteria.

---
+++ largerThan.sql
@@ -0,0 +1,9 @@
+Select *
+from film 
+where replacement_cost > 10.00; 
+
+
+-- Here we are retrieving data of employee whose salary is more than 5000
+Select * from 
+Employee 
+where salary > 5000;
