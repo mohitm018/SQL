94975c98a6999179099c5bc8b4165f3d86158378
mohitm018 <91406180+mohitm018@users.noreply.github.com>
2026-09-18 17:25:59 +0530

    Create SQL script for left join of employees and departments
    This SQL script performs a left join between the employee and department tables, ensuring all employees are included in the result set, even those without an assigned department.

---
+++ LeftJoinEmployeeDepartment.sql
@@ -0,0 +1,12 @@
+
+
+
+
+
+Select *
+from employee as e
+left join department as d
+on e.department_id= d.department_id; 
+
+
+-- All date from employeee even the one who hasnt assign ani departmentt will be printed as NULL
